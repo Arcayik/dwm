09f98c86e6599c452d954fcd732c3bfd0f570602
Blakely Haughton <blakely.haughton@gmail.com>
2025-11-12 15:02:17 -0500

    change tag square into tag underline

--- config.def.h
+++ config.def.h
@@ -22,6 +22,11 @@ static const char *colors[][3]      = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
+static const unsigned int ulinepad	= 8;	/* horizontal padding between the underline and tag */
+static const unsigned int ulinepadsel = 5;  /* horizontal padding when selected */
+static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
+static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
+
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
